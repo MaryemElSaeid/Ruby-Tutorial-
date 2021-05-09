223818d3ef8c2180aa22f26421548bea96e704b3
MaryemElSaeid <maryemelsaeid@gmail.com>
2021-05-09 17:37:59 +0200

    lab3

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,59 @@
 class ArticlesController < ApplicationController
-    def new 
-    end 
+  
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+  
+  def index
+      @articles = Article.all
+    end
+
+
+    def show
+      @article = Article.find(params[:id])
+    end
+
+
+    def new
+      @article = Article.new
+    end
+  
 
     def create
+      @article = Article.new(article_params)
+  
+      if @article.save
+        redirect_to @article
+      else
+        render :new
+      end
+    end
+
+
+    
+def edit
+  @article = Article.find(params[:id])
+end
+
+def update
+  @article = Article.find(params[:id])
+
+  if @article.update(article_params)
+    redirect_to @article
+  else
+    render :edit
+  end
+end
+
+
+
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+
+  redirect_to @article
+end
+
+private
+    def article_params
+      params.require(:article).permit(:title, :text, :status)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  resources :articles 
- 
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root "articles#index"
+
+  resources :articles do
+    resources :comments
+  end
+
+  
 end
